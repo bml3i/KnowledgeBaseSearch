05cfa911e75820c3c72c0a6aaf253418253ef648
Leo Bi <biminglei@gmail.com>
2025-08-19 18:55:50 +0800

    support sslmode=require

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,23 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
+// 构建数据库配置
+const dbConfig = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-});
+};
+
+// 如果设置了SSL模式，添加SSL配置
+if (process.env.DB_SSL_MODE === 'require') {
+  dbConfig.ssl = {
+    require: true,
+    rejectUnauthorized: process.env.DB_SSL_REJECT_UNAUTHORIZED !== 'false'
+  };
+}
+
+const pool = new Pool(dbConfig);
 
 pool.on('connect', () => {
   console.log('Connected to the database');
